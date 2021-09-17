e21ab14d0d584effbd7518ff166db1561ba89d60
hrudesh-lenka <hrudesh.lenka@gmail.com>
2021-09-17 19:53:13 +0530

    aadded skills

--- src/components/data/skills.js
+++ src/components/data/skills.js
@@ -1,7 +1,52 @@
+// // devicon.dev
+// export const SkillsData = [
+//     {
+//         type: "Frontend",
+//         list: [
+//             {
+//                 name: 'HTML',
+//                 icon: <i class="devicon-html5-plain colored"></i>
+//             },
+//             {
+//                 name: 'CSS3',
+//                 icon: <i class="devicon-css3-plain colored"></i>
+//             },
+//             {
+//                 name: 'ReactJs',
+//                 icon: <i class="devicon-react-original colored"></i>
+//             },
+//         ],
+//     },{
+//         type: "Backend",
+//         list: [
+//             {
+//                 name: 'NodeJs',
+//                 icon: <i class="devicon-nodejs-plain colored"></i>
+//             },
+//             {
+//                 name: 'Express.js',
+//                 icon: <i class="devicon-express-original colored"></i>
+//             },
+            
+//         ],
+//     },{
+//         type: "Database",
+//         list: [
+//             {
+//                 name: 'MongoDB',
+//                 icon: <i class="devicon-mongodb-plain colored"></i>
+//             },
+//             {
+//                 name: 'PostgreSQL',
+//                 icon: <i class="devicon-postgresql-plain colored"></i>
+//             },
+            
+//         ],
+//     }
+// ]
 // devicon.dev
 export const SkillsData = [
     {
-        type: "Frontend",
         list: [
             {
                 name: 'HTML',
@@ -15,10 +60,6 @@ export const SkillsData = [
                 name: 'ReactJs',
                 icon: <i class="devicon-react-original colored"></i>
             },
-        ],
-    },{
-        type: "Backend",
-        list: [
             {
                 name: 'NodeJs',
                 icon: <i class="devicon-nodejs-plain colored"></i>
@@ -27,11 +68,6 @@ export const SkillsData = [
                 name: 'Express.js',
                 icon: <i class="devicon-express-original colored"></i>
             },
-            
-        ],
-    },{
-        type: "Database",
-        list: [
             {
                 name: 'MongoDB',
                 icon: <i class="devicon-mongodb-plain colored"></i>
@@ -40,7 +76,18 @@ export const SkillsData = [
                 name: 'PostgreSQL',
                 icon: <i class="devicon-postgresql-plain colored"></i>
             },
-            
-        ],
+            {
+                name: 'Docker',
+                icon: <i class="devicon-docker-plain colored"></i>
+            },
+            {
+                name: 'Python',
+                icon: <i class="devicon-python-plain colored"></i>
+            },
+            {
+                name: 'Java',
+                icon: <i class="devicon-java-plain colored"></i>
+            }
+        ]
     }
 ]
